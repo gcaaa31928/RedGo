0c04e9c4745ed498dd23cbeb78c9f932abe79acd
gcaaa31928 <gcaaa31928@gmail.com>
2017-01-10 03:09:47 +0800

    prototyping board

--- redgo/board.py
+++ redgo/board.py
@@ -1,13 +1,40 @@
 from .sgf_parser import sgf
+from enum import Enum
+
+
+class Color(Enum):
+    black = 0
+    white = 1
+
+
+class Chain:
+    def __init__(self, color):
+        self.color = color
+        self.stones = []
+        self.liberties = []
+
+    def add_stone(self, pos):
+        self.stones.append(pos)
+
+    def add_liberty(self, pos):
+        self.liberties.append(pos)
 
 
 class Board:
-    def __init__(self):
+    def __init__(self, size):
+        self.board = {}
+        self.size = size
+        self.chains = {}
         pass
 
-    @staticmethod
-    def create_board(sgf_content):
-        print(type(sgf_content))
-        sgf_game = sgf.Sgf_game.from_string(sgf_content)
-        print(sgf_game)
-        return Board()
+    def create_chain(self, color, pos):
+        chain = Chain(color)
+
+    def merge_adjacent_chain(self, chain):
+        pass
+
+    def move(self, color, pos):
+        pass
+
+    def get_features(self, color, move):
+        pass
--- redgo/processor.py
+++ redgo/processor.py
@@ -1,10 +1,65 @@
-from redgo.board import Board
+import fnmatch
+
+from redgo.board import *
 from .definitions import *
+import zipfile
+
+
+def find_files(directory, pattern='*.zip'):
+    '''Recursively finds all files matching the pattern.'''
+    files = []
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, pattern):
+            files.append(os.path.join(root, filename))
+    return files
 
 
 class Processor:
+    def __init__(self):
+        self.sgf_dir = os.path.join(ROOT_DIR, './kgs/')
 
-    @staticmethod
-    def read_sgf_file(file_path):
-        with open(file_path, 'r') as content_file:
-            content = bytes(content_file.read(), encoding='utf8')
+    def handicap(self, board, sgf_game):
+        if sgf_game.get_handicap():
+            for move in sgf_game.get_root().get_setup_stones()[0]:
+                board.move(Color.black, move)
+
+    # def read_sgf_file_to_boards(self, file_path):
+    #     boards = []
+    #     with open(file_path, 'r') as content_file:
+    #         content = bytes(content_file.read(), encoding='utf8')
+
+    def derive_sgf_content(self, sgf_content):
+        sgf_game = sgf.Sgf_game.from_string(sgf_content)
+        board = Board(19)
+        board_problems = []
+        board_solution = []
+        self.handicap(board, sgf_game)
+        for node in sgf_game.main_sequence_iter():
+            color, move = node.get_move()
+            problem, solution = board.get_features(color, move)
+            board_problems.append(problem)
+            board_solution.append(solution)
+            board.move(color, move)
+        return board_problems, board_solution
+
+    def get_total_samples_in_zip(self, current_zip_file):
+        current_zip = zipfile.ZipFile(current_zip_file)
+        file_names = current_zip.namelist()
+        all_probs = []
+        all_sols = []
+        for name in file_names:
+            if name.endswith('.sgf'):
+                sgf_content = current_zip.read(name)
+                probs, sols = self.derive_sgf_content(sgf_content)
+                all_probs.extend(probs)
+                all_sols.extend(sols)
+        return all_probs, all_sols
+
+    def get_total_samples_in_dir(self):
+        sgf_zips = find_files(self.sgf_dir)
+        all_probs = []
+        all_sols = []
+        for sgf_zip in sgf_zips:
+            probs, sols = self.get_total_samples_in_zip(sgf_zip)
+            all_probs.extend(probs)
+            all_sols.extend(sols)
