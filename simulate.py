4f4042d5a7e2bb6a0fed7bb78dba57fe81c6e4a0
gcaaa31928 <gcaaa31928@gmail.com>
2017-01-14 16:56:40 +0800

    added simulate and downloader

--- simulate.py
+++ simulate.py
@@ -1,14 +1,15 @@
+from time import sleep
+import numpy as np
 import h5py
 import tflearn
 from tflearn import *
 import tensorflow as tf
 
-h5f = h5py.File('data.h5', 'r')
-X = h5f['X']
-Y = h5f['Y']
+from redgo.board import Board, Color
+
 batch_size = 128
 n_classes = 19 * 19
-# Build network
+
 network = input_data(shape=[None, 19, 19, 7], dtype=tf.float32)
 network = conv_2d(network, 48, 3, activation='relu')
 network = conv_2d(network, 32, 3, activation='relu')
@@ -21,10 +22,17 @@ network = regression(network, optimizer='sgd',
                      loss='categorical_crossentropy',
                      learning_rate=0.05)
 
-# Training
 model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='./log', max_checkpoints=1)
-model.load('log-166500')
-model.fit(X, Y, n_epoch=100, shuffle=True, validation_set=0.1, snapshot_step=500,
-          show_metric=True, batch_size=batch_size, run_id='redgo5')
+model.load('./log-333000')
 
-h5f.close()
+board = Board(size=19)
+current_color = Color.white
+while True:
+    current_color = Color.white if current_color == Color.black else Color.black
+    probs = board.get_features(current_color, None)[0]
+    probs = np.reshape(probs, [1, 19, 19, 7])
+    result = np.argmax(model.predict(probs)[0])
+    move = board.decode_move(result)
+    board.move(current_color, move)
+    print(str(board))
+    sleep(3)
