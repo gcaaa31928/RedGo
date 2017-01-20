59b56d60f7c06973b6b91e5c11b567aa45272597
gcaaa31928 <gcaaa31928@gmail.com>
2017-01-20 18:58:27 +0800

    changed train input network size

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ Y = h5f['Y']
 batch_size = 128
 n_classes = 19 * 19
 # Build network
-network = input_data(shape=[None, 19, 19, 7], dtype=tf.float32)
+network = input_data(shape=[None, 19, 19, 8], dtype=tf.float32)
 network = conv_2d(network, 256, 7, activation='relu')
 network = conv_2d(network, 256, 5, activation='relu')
 network = conv_2d(network, 256, 5, activation='relu')
